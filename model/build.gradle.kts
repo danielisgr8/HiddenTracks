f5ce00a354c1da56febd406d48b5d064bf232277
Daniel Schubert <me@danielschubert.dev>
2024-04-27 01:28:58 -0700

    Client: Upgrade dependencies, clean config

--- model/build.gradle.kts
+++ model/build.gradle.kts
@@ -1,31 +1,25 @@
 plugins {
-    id("software.amazon.smithy").version("0.6.0")
+    id("software.amazon.smithy").version("0.7.0")
 }
 
 repositories {
-    mavenLocal()
     mavenCentral()
 }
 
 dependencies {
-    implementation("software.amazon.smithy:smithy-model:1.37.0")
-    implementation("software.amazon.smithy.typescript:smithy-typescript-codegen:0.17.1")
-    implementation("software.amazon.smithy:smithy-aws-traits:1.39.1")
+    implementation("software.amazon.smithy:smithy-model:1.47.0")
+    implementation("software.amazon.smithy.typescript:smithy-typescript-codegen:0.19.0")
+    implementation("software.amazon.smithy:smithy-aws-traits:1.47.0")
 }
 
 configure<software.amazon.smithy.gradle.SmithyExtension> {
-    // Uncomment this to use a custom projection when building the JAR.
     // projection = "foo"
 }
 
 buildscript {
-    repositories {
-        mavenCentral()
-    }
     dependencies {
-        classpath("software.amazon.smithy.typescript:smithy-aws-typescript-codegen:0.12.0")
+        classpath("software.amazon.smithy.typescript:smithy-aws-typescript-codegen:0.19.0")
     }
 }
 
-// Uncomment to disable creating a JAR.
-//tasks["jar"].enabled = false
+tasks["jar"].enabled = false
