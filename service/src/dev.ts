4c4a070c3dfc6bee00f0c362e51bafe9186e2458
Daniel Schubert <me@danielschubert.dev>
2024-12-25 04:17:36 -0800

    Service: Initial implementation. Dev script.

---
+++ service/src/dev.ts
@@ -0,0 +1,53 @@
+/**
+ * File used for local development.
+ * Fill out the variables before `input` and run this file via `npm run dev`.
+ */
+
+import { APIGatewayProxyEventV2 } from 'aws-lambda';
+import { lambdaHandler } from '.';
+
+const method = 'GET';
+const path = '/list-conversations/daniel';
+const pathParameters = {
+  user: 'daniel',
+};
+const body = undefined;
+
+const input: APIGatewayProxyEventV2 = {
+  headers: {
+    'header1': 'value1',
+  },
+  queryStringParameters: {
+    'parameter1': 'value1',
+  },
+  requestContext: {
+    accountId: '123456789012',
+    apiId: 'id',
+    domainName: 'id.execute-api.us-east-1.amazonaws.com',
+    domainPrefix: 'id',
+    http: {
+      method: method,
+      path,
+      protocol: 'HTTPS',
+      sourceIp: '127.0.0.1',
+      userAgent: '',
+    },
+    requestId: 'id=',
+    routeKey: '',
+    stage: '$default',
+    time: '2024-12-25T11:58:37.545Z',
+    timeEpoch: 1735127900,
+  },
+  pathParameters,
+  body,
+  isBase64Encoded: false,
+  version: '2.0',
+  routeKey: '',
+  rawPath: path,
+  rawQueryString: '',
+};
+
+(async () => {
+  const output = await lambdaHandler(input);
+  console.log('Handler output: ', output);
+})();
--- service/src/index.ts
+++ service/src/index.ts
@@ -1,7 +1,31 @@
-import { ListMessagesInput } from "@danielisgr8/hidden-tracks-service-ssdk";
+import { convertEvent, convertResponse } from "@aws-smithy/server-apigateway";
+import { getHiddenTracksServiceHandler, HiddenTracksService, ListConversations, ListConversationsInput, ListConversationsOutput } from "@danielisgr8/hidden-tracks-service-ssdk";
+import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from "aws-lambda";
 
-const foo: ListMessagesInput = {
-  user: 'blah',
-  conversationId: '123',
+type RequestContext = object;
+
+const listConversationsHandler: ListConversations<RequestContext> = async (input: ListConversationsInput): Promise<ListConversationsOutput> => {
+  console.log(`Listing conversations for ${input.user}`);
+  return {
+    items: [{ conversationId: '123', recipients: ['daniel'], encodingFormat: '1' }],
+  };
 };
-console.log(JSON.stringify(foo, null, 2));
+
+// @ts-expect-error TODO: Implement remaining operations
+const hiddenTracksService: HiddenTracksService<RequestContext> = {
+  ListConversations: listConversationsHandler,
+}
+
+const serviceHandler = getHiddenTracksServiceHandler(hiddenTracksService);
+
+export const lambdaHandler = async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> => {
+  console.log('Input: ', event);
+
+  const convertedEvent = convertEvent(event);
+  const rawResponse = await serviceHandler.handle(convertedEvent, {});
+  const convertedResponse = convertResponse(rawResponse);
+
+  console.log('Output: ', convertedResponse);
+
+  return convertedResponse;
+}
