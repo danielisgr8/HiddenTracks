be5b2359b8593ede3d587c5d121f0798133ce43b
Daniel Schubert <me@danielschubert.dev>
2025-01-18 00:18:13 -0800

    Service: Dummy implementation of operations

--- service/src/index.ts
+++ service/src/index.ts
@@ -1,20 +1,76 @@
 import { convertEvent, convertResponse } from "@aws-smithy/server-apigateway";
-import { getHiddenTracksServiceHandler, HiddenTracksService, ListConversations, ListConversationsInput, ListConversationsOutput } from "@danielisgr8/hidden-tracks-service-ssdk";
+import {
+  CreateConversation,
+  CreateConversationServerInput,
+  CreateConversationServerOutput,
+  getHiddenTracksServiceHandler,
+  HiddenTracksService,
+  ListConversations,
+  ListConversationsServerInput,
+  ListConversationsServerOutput,
+  ListMessages,
+  ListMessagesServerInput,
+  ListMessagesServerOutput,
+  ListSongsForEncoding,
+  ListSongsForEncodingServerOutput,
+  SendMessage,
+  SendMessageServerInput,
+  SendMessageServerOutput,
+} from "@danielisgr8/hidden-tracks-service-ssdk";
 import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from "aws-lambda";
 
 type RequestContext = object;
 
-const listConversationsHandler: ListConversations<RequestContext> = async (input: ListConversationsInput): Promise<ListConversationsOutput> => {
+const listConversationsHandler: ListConversations<RequestContext> = async (input: ListConversationsServerInput): Promise<ListConversationsServerOutput> => {
   console.log(`Listing conversations for ${input.user}`);
   return {
-    items: [{ conversationId: '123', recipients: ['daniel'], encodingFormat: '1' }],
+    items: [{ conversationId: '123', recipients: ['daniel', 'greg'], encodingFormat: '1' }],
   };
 };
 
-// @ts-expect-error TODO: Implement remaining operations
+const createConversationHandler: CreateConversation<RequestContext> = async (input: CreateConversationServerInput): Promise<CreateConversationServerOutput> => {
+  return { conversationId: '234', recipients: input.recipients, encodingFormat: input.encodingFormat };
+};
+
+const sendMessageHandler: SendMessage<RequestContext> = async (input: SendMessageServerInput): Promise<SendMessageServerOutput> => {
+  return {
+    messageId: '345',
+    conversationId: input.conversationId,
+    user: input.user,
+    createTime: new Date(),
+    playlistId: input.playlistId,
+    startOffset: input.startOffset,
+    endOffset: input.endOffset,
+  };
+};
+
+const listMessagesHandler: ListMessages<RequestContext> = async (input: ListMessagesServerInput): Promise<ListMessagesServerOutput> => {
+  return {
+    items: [{
+      messageId: '456',
+      conversationId: input.conversationId,
+      user: input.user,
+      createTime: new Date(),
+      playlistId: '123ABCabc',
+      startOffset: 0,
+      endOffset: 5,
+    }],
+  };
+};
+
+const listSongsForEncodingHandler: ListSongsForEncoding<RequestContext> = async (): Promise<ListSongsForEncodingServerOutput> => {
+  return {
+    songs: ['spotify:track:123ABCabc'],
+  };
+};
+
 const hiddenTracksService: HiddenTracksService<RequestContext> = {
   ListConversations: listConversationsHandler,
-}
+  CreateConversation: createConversationHandler,
+  SendMessage: sendMessageHandler,
+  ListMessages: listMessagesHandler,
+  ListSongsForEncoding: listSongsForEncodingHandler,
+};
 
 const serviceHandler = getHiddenTracksServiceHandler(hiddenTracksService);
 
@@ -28,4 +84,4 @@ export const lambdaHandler = async (event: APIGatewayProxyEventV2): Promise<APIG
   console.log('Output: ', convertedResponse);
 
   return convertedResponse;
-}
+};
